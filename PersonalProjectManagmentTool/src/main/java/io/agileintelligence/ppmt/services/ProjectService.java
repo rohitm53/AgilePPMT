f69fc5fd4c1207c98357b32861a3a324927336ad
rohitm53 <36997587+rohitm53@users.noreply.github.com>
2019-09-28 23:08:23 +0530

    Development Branch made

--- PersonalProjectManagmentTool/src/main/java/io/agileintelligence/ppmt/services/ProjectService.java
+++ PersonalProjectManagmentTool/src/main/java/io/agileintelligence/ppmt/services/ProjectService.java
@@ -19,7 +19,27 @@ public class ProjectService {
         }catch (Exception e){
             throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already existed");
         }
-
     }
+
+    public Project findByProjectIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project==null){
+            throw new ProjectIdException("Poject ID '"+projectId+"' does not exist");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project==null){
+            throw new ProjectIdException("Cannot delete Project with ID '"+projectId+"'. This project does not exist");
+        }
+        projectRepository.delete(project);
+    }
+
 
 }
--- PersonalProjectManagmentTool/src/main/java/io/agileintelligence/ppmt/web/ProjectController.java
+++ PersonalProjectManagmentTool/src/main/java/io/agileintelligence/ppmt/web/ProjectController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -36,5 +33,22 @@ public class ProjectController {
         }
         projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+        Project project = projectService.findByProjectIdentifier(projectId);
+        return new ResponseEntity<Project>(project,HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects(){
+        return projectService.findAllProjects();
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<String>("Project with ID : "+projectId+" was deleted",HttpStatus.OK);
     }
 }
