839139666fa2a24f3da143780a60f428c470bea0
rohitm53 <36997587+rohitm53@users.noreply.github.com>
2019-10-02 22:53:03 +0530

    redux

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -6,18 +6,21 @@ import AddProject from './components/Project/AddProject'
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'font-awesome/css/font-awesome.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/addProject" component={AddProject} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
